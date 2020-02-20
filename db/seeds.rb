24c18785dbc8a7ade1472bf4892ca2e81ef7fae2
Dani P <daniela.s.paino@gmail.com>
2020-02-20 12:58:35 +1100

    passes rake, destroy raise error more explicit way but not needed to pass rake

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 require 'json'
-require 'rest-client'
+require 'open-uri'
 
 Ingredient.destroy_all if Rails.env.development?
 
-ingredients = []
-# TODO: seed to insert 10 posts in the database fetched from the Hacker News API.
-response = RestClient.get 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_data = open(url).read
+ingredients_parsed = JSON.parse(ingredients_data)
+ingredients_parsed = ingredients_parsed['drinks']
 
-repos = JSON.parse(response)
-
-first_twenty = repos['drinks'].first(20)
-
-first_twenty.each do |ingredient|
-  ingredient = Ingredient.new(name: ingredient['strIngredient1'])
+ingredients_parsed.each do |i|
+  ingredient = Ingredient.new(name: i['strIngredient1'])
   ingredient.save!
-  ingredients << ingredient
 end
-
-ingredients
