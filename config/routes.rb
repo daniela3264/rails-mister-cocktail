9c1bd94ed5eeb7570045ea3193a00afe6e576f2c
Dani P <daniela.s.paino@gmail.com>
2020-02-20 17:16:21 +1100

    all rakes pass, workingon view and forms for cocktails doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # root to 'home' home page?
+  get 'cocktails', to: 'cocktails#index'
+  resources :cocktails, only: %i[new create show] do |r|
+    resources :doses, only: %i[new create]
+  end
+
+  resources :doses, only: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
