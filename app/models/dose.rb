de9f551845f1f426ff4e7486573733997d308fde
Dani P <daniela.s.paino@gmail.com>
2020-02-20 12:29:20 +1100

    migrate tables schema

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :description, presence:true
+  validates :ingredient, uniqueness: { scope: :cocktail,
+    message: "An ingredient should only be included once per cocktail" }
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,4 @@
 class Ingredient < ApplicationRecord
   has_many :doses, dependent: :restrict_with_exception
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
