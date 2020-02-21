5aaff30dc607fe6861d1f13c35e615889f456d36
Dani P <daniela.s.paino@gmail.com>
2020-02-21 11:27:35 +1100

    Add dotenv - Protect my secret data in .env file

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
+gem 'dotenv-rails', groups: [:development, :test]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # root to 'home' home page?
+  resources :contact, only: [:new, :create]
   get 'cocktails', to: 'cocktails#index'
   resources :cocktails, only: %i[new create show] do |r|
     resources :doses, only: %i[new create]
   end
 
-  resources :doses, only: [:destroy]
+  resources :doses, only: [:destroy] # do if can add ingredients.
+    #  resources :ingredients [only :new, :create]
+  # end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# add plant_tag to a plant in garden.
+
